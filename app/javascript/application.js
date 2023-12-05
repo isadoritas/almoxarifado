b6005701436be38fbebf56af81fb69e14584c9fc
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-12-05 14:29:50 -0300

    Add styling to log history

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import 'bootstrap/dist/css/bootstrap'
-import 'bootstrap/dist/js/bootstrap'
 import "controllers"
-import "stylesheets/application"
 
